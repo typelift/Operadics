68e800480261ca4a15d8d3218abcf0d437754930
Robert Widmann <devteam.codafi@gmail.com>
2016-09-18 13:32:04 -0400

    Output dylibs

--- Package.swift
+++ Package.swift
@@ -2,3 +2,5 @@ import PackageDescription
 
 let package = Package(name: "Operadics")
 
+let operadics = Product(name: "Operadics", type: .Library(.Dynamic), modules: "Operadics")
+products.append(operadics)
